9f46527899854fb7536c81587fad2ebba66c5934
drking445 <drking445@gmail.com>
2017-11-15 01:35:50 -0500

    Adding separate endpoints files

---
+++ conference/HelloClass.java
@@ -0,0 +1,20 @@
+package com.google.training.helloworld;
+
+public class HelloClass {
+    public String message = "Hello Endpoints";
+
+    public HelloClass () {
+    }
+
+    public HelloClass (String name) {
+        message = "Hello " + name + "!";
+    }
+  
+    public HelloClass(String name, String period ) {
+        message = "Good " + period + " " + name;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
